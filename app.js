14e5b0e8ecaee469bb62e74f59e71e8f664a5d6b
Pétala Leite <petalaleite@gmail.com>
2023-03-25 17:35:31 -0300

    feat: Add genre model and routes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const genreRouter = require('./src/routes/genre.routes')
 const app = express()
 
 app.use(express.json())
+app.use('/genres', genreRouter)
 
 
 module.exports = app
