e4fea8039bfbeea9b451f7a1ad8452af00fb50fa
Pétala Leite <petalaleite@gmail.com>
2023-04-07 16:22:50 -0300

    feat: add customer model schema

---
+++ src/models/customer.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const Customer = model('Customer', new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50
+  },
+  premiumAccount: {
+    type: Boolean,
+    default: false,
+  },
+  address: {
+    type: String,
+    required: true,
+    minlength: 20,
+    maxlength: 255
+  },
+  phone: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+}));
+
+module.exports = Customer;
