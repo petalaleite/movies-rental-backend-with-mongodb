9341ff48630eadcb36e9c5f67450064ab4921d2b
Pétala Leite <petalaleite@gmail.com>
2023-04-09 20:36:18 -0300

    feat: add middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { registerUser, deleteUser } = require('../controllers/userControllers');
+const validateToken = require('../middlewares/validateJWT');
+const admin = require('../middlewares/admin');
 
 const userRouter = express.Router();
 
 userRouter.post('/register', registerUser);
-userRouter.delete('/:id', deleteUser);
+userRouter.delete('/:id', [validateToken, admin], deleteUser);
 
 module.exports = userRouter;
