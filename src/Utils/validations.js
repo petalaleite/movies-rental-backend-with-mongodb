062a1e9c53674f8c06d56235ca90d3b090cd4dbf
Pétala Leite <petalaleite@gmail.com>
2023-04-15 21:33:54 -0300

    FEAT: create movie middleware validation

--- src/Utils/validations.js
+++ src/Utils/validations.js
@@ -4,10 +4,10 @@ const genre = Joi.object({
   name: Joi.string().min(2).required(),
 });
 
-const movie = {
+const movie = Joi.object({
   title: Joi.string().min(2).max(255).required(),
   genreId: Joi.string().required(),
   numberInStock: Joi.number().min(0).required(),
-};
+});
 
 module.exports = { genre, movie };
--- src/middlewares/validations.js
+++ src/middlewares/validations.js
@@ -9,4 +9,12 @@ const validateGenre = (req, res, next) => {
   next();
 };
 
-module.exports = { validateGenre };
+const validateMovie = (req, res, next) => {
+  const { body } = req;
+  const { error } = Joi.movie.validate(body);
+
+  if (error) return res.status(statusCodes.BAD_REQUEST).json({ message: error.message });
+  next();
+};
+
+module.exports = { validateGenre, validateMovie };
